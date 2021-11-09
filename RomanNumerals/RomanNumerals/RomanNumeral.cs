b178a3b8c6bb0ddac8506c9c1ea70d098bb1556c
Steven Yackel <styackel@microsoft.com>
2021-11-09 16:57:42 -0600

    add roman numerals

---
+++ RomanNumerals/RomanNumerals/RomanNumeral.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    /*
+ Write a function that, given a string representing a roman numeral, converts it to an integer.
+
+Symbol:
+I -> 1
+V -> 5
+X -> 10
+L -> 50
+C -> 100
+D -> 500
+M -> 1000
+
+
+Roman numerals leverage a subtractive notation for certain values
+4, 9: IV, IX
+40, 90: XL, XC
+400, 900: CD, CM
+     */
+    public class RomanNumeral
+    {
+        public static string ToRomanNumeral(int n)
+        {
+            var result = new StringBuilder();
+
+            while (n > 0)
+            {
+                if (n >= 1000)
+                {
+                    result.Append("M");
+                    n -= 1000;
+                }
+                else if (n >= 900)
+                {
+                    result.Append("CM");
+                    n -= 900;
+                }
+                else if (n >= 500)
+                {
+                    result.Append("D");
+                    n -= 500;
+                }
+                else if (n >= 400)
+                {
+                    result.Append("CD");
+                    n -= 400;
+                }
+                else if (n >= 100)
+                {
+                    result.Append("C");
+                    n -= 100;
+                }
+                else if (n >= 90)
+                {
+                    result.Append("XC");
+                    n -= 90;
+                }
+                else if (n >= 50)
+                {
+                    result.Append("L");
+                    n -= 50;
+                }
+                else if (n >= 40)
+                {
+                    result.Append("XL");
+                    n -= 40;
+                }
+                else if (n >= 10)
+                {
+                    result.Append("X");
+                    n -= 10;
+                }
+                else if (n >= 9)
+                {
+                    result.Append("IX");
+                    n -= 9;
+                }
+                else if (n >= 5)
+                {
+                    result.Append("V");
+                    n -= 5;
+                }
+                else if (n >= 4)
+                {
+                    result.Append("IV");
+                    n -= 4;
+                }
+                else if (n >= 1)
+                {
+                    result.Append("I");
+                    n -= 1;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static List<Tuple<string, int>> RNValues = new()
+        {
+            new Tuple<string, int>("M", 1000),
+            new Tuple<string, int>("CM", 900),
+            new Tuple<string, int>("D", 500),
+            new Tuple<string, int>("CD", 400),
+            new Tuple<string, int>("C", 100),
+            new Tuple<string, int>("XC", 90),
+            new Tuple<string, int>("L", 50),
+            new Tuple<string, int>("XL", 40),
+            new Tuple<string, int>("X", 10),
+            new Tuple<string, int>("IX", 9),
+            new Tuple<string, int>("V", 5),
+            new Tuple<string, int>("IV", 4),
+            new Tuple<string, int>("I", 1),
+        };
+
+        public static int ToInt(string rn)
+        {
+            ArgumentNullException.ThrowIfNull(rn);
+            var result = 0;
+
+            while (rn.Length > 0)
+            {
+                foreach (var rnValue in RNValues)
+                {
+                    if (rn.StartsWith(rnValue.Item1))
+                    {
+                        result += rnValue.Item2;
+                        rn = rn.Substring(rnValue.Item1.Length);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumerals/TestRomanNumeral.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class TestRomanNumeral
+    {
+
+        [TestMethod]
+        public void IntToRomanNumeral()
+        {
+            Assert.AreEqual("I", RomanNumeral.ToRomanNumeral(1));
+            Assert.AreEqual("II", RomanNumeral.ToRomanNumeral(2));
+            Assert.AreEqual("IV", RomanNumeral.ToRomanNumeral(4));
+            Assert.AreEqual("XXXIX", RomanNumeral.ToRomanNumeral(39));
+            Assert.AreEqual("CCXLVI", RomanNumeral.ToRomanNumeral(246));
+            Assert.AreEqual("DCCLXXXIX", RomanNumeral.ToRomanNumeral(789));
+            Assert.AreEqual("MMCDXXIII", RomanNumeral.ToRomanNumeral(2423));
+        }
+
+        [TestMethod]
+        public void RomanNumeralToInt()
+        {
+            Assert.AreEqual(1, RomanNumeral.ToInt("I"));
+            Assert.AreEqual(2, RomanNumeral.ToInt("II"));
+            Assert.AreEqual(4, RomanNumeral.ToInt("IV"));
+            Assert.AreEqual(39, RomanNumeral.ToInt("XXXIX"));
+            Assert.AreEqual(246, RomanNumeral.ToInt("CCXLVI"));
+            Assert.AreEqual(789, RomanNumeral.ToInt("DCCLXXXIX"));
+            Assert.AreEqual(2423, RomanNumeral.ToInt("MMCDXXIII"));
+        }
+
+        [TestMethod]
+        public void TwoWayConversion()
+        {
+            for (int i = 1; i <= 3999; i++)
+            {
+                var romanNumeral = RomanNumeral.ToRomanNumeral(i);
+
+                Assert.AreEqual(i, RomanNumeral.ToInt(romanNumeral));
+            }
+        }
+    }
+}
