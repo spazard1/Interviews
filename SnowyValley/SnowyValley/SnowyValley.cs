efbd87c4f8bfbab274f6c1cd0c8733c0e0e1af9a
Steven Yackel <styackel@microsoft.com>
2021-11-11 16:02:19 -0600

    adds base snowy valley

---
+++ SnowyValley/SnowyValley/SnowyValley.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    /*
+
+    */
+    public class SnowyValley
+    {
+        public static int CountSnow(int[] peaks)
+        {
+            for (var i = 0; i < peaks.Length; i++)
+            {
+
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ SnowyValley/SnowyValley/TestSnowyValley.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class TestSnowyValley
+    {
+
+        [TestMethod]
+        public void IntToRomanNumeral()
+        {
+            Assert.AreEqual("I", SnowyValley.ToRomanNumeral(1));
+            Assert.AreEqual("II", SnowyValley.ToRomanNumeral(2));
+            Assert.AreEqual("IV", SnowyValley.ToRomanNumeral(4));
+            Assert.AreEqual("XXXIX", SnowyValley.ToRomanNumeral(39));
+            Assert.AreEqual("CCXLVI", SnowyValley.ToRomanNumeral(246));
+            Assert.AreEqual("DCCLXXXIX", SnowyValley.ToRomanNumeral(789));
+            Assert.AreEqual("MMCDXXIII", SnowyValley.ToRomanNumeral(2423));
+        }
+
+        [TestMethod]
+        public void RomanNumeralToInt()
+        {
+            Assert.AreEqual(1, SnowyValley.ToInt("I"));
+            Assert.AreEqual(2, SnowyValley.ToInt("II"));
+            Assert.AreEqual(4, SnowyValley.ToInt("IV"));
+            Assert.AreEqual(39, SnowyValley.ToInt("XXXIX"));
+            Assert.AreEqual(246, SnowyValley.ToInt("CCXLVI"));
+            Assert.AreEqual(789, SnowyValley.ToInt("DCCLXXXIX"));
+            Assert.AreEqual(2423, SnowyValley.ToInt("MMCDXXIII"));
+        }
+
+        [TestMethod]
+        public void TwoWayConversion()
+        {
+            for (int i = 1; i <= 3999; i++)
+            {
+                var romanNumeral = SnowyValley.ToRomanNumeral(i);
+
+                Assert.AreEqual(i, SnowyValley.ToInt(romanNumeral));
+            }
+        }
+    }
+}
